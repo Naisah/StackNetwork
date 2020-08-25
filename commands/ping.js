241f2ff27fa534501ede7d4df6a2d45d496bca32
Naisah <naisahaspiras24@gmail.com>
2020-08-25 21:06:44 +0800

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,12 @@ module.exports = {
     execute(message, args){
         message.channel.send('pong!');
 
-        
+        client.on('message', msg => {
+            if (msg.content === 'ping') {
+              msg.reply('pong');
+            }
+          });
+          
 
 
     }
