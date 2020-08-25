4b9d4388fa2eca9dc904a476556ce63f8bd20e33
Naisah <naisahaspiras24@gmail.com>
2020-08-25 21:19:53 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '/' ;
 const fs = require('fs');
+const embed = new Discord.MessageEmbed()
 
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
@@ -34,17 +35,12 @@ client.on('message', message =>{
     if (command === 'vote')
         message.channel.send(' 1.https://minecraft-server-list.com/server/465627/vote/ 2.https://www.planetminecraft.com/server/stacknetwork/vote/ 3.https://minecraft-mp.com/server/265382/vote/ 4.https://www.serverpact.com/vote-44084 5.https://minecraftservers.org/vote/591911 6.https://serverlist101.com/server/2003/vote/ 7.https://topg.org/Minecraft/in-612558' )
 
-
-
-
-
     if (command === 'Embed') {
         client.commands.get('Embed').execute(message, args);
     } 
     
 
 })
-
 
 client.login(process.env.token);
 
