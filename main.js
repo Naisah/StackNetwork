8159f0847f516c0df0e230485ca13161ce3f730f
Naisah <naisahaspiras24@gmail.com>
2020-08-25 22:44:20 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,15 +2,18 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '/' ;
 const fs = require('fs');
-const embed = new Discord.MessageEmbed()
 
 client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
+const embed = new Discord.MessageEmbed()
 
-    client.commands.set(command.name, command);
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+
+  client.commands.set(command.name, command);
 }
+
+
 
 const moment = require('moment')
 const { MessageEmbed } = require('discord.js')
